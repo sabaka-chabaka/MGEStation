96f5f6d11bd8b95f982a7965b191cac811c304f5
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-16 13:03:45 +0400

    ts code

--- MGE-Station/src/main.ts
+++ MGE-Station/src/main.ts
@@ -1,1 +1,10 @@
 import './style.css'
+const playBtn = document.getElementById('playBtn') as HTMLButtonElement | null;
+
+playBtn?.addEventListener('click', () => {
+    if (playBtn.innerHTML.includes('fa-play')) {
+        playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+    } else {
+        playBtn.innerHTML = '<i class="fas fa-play"></i>';
+    }
+});
